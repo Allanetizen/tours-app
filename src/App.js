cf6eda8ae18ae32b8f297a69bb03217829ce6ac8
Allan <allankosgei747@gmail.com>
2022-02-27 17:58:06 +0300

    added a navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import SearchAppBar from './components/AppBar';
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar/>
+      <Container  sx={{
+        marginY:5
+      }}>
         <Grid container spacing={3}>
           <TourCard />
           <TourCard />
