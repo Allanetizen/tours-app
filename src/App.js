12bb448e19e3a806bd277dcb3b04a2e8f80ff576
Allan <allankosgei747@gmail.com>
2022-02-27 15:12:10 +0300

    Card component, grid and container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import TourCard from "./components/TourCard";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div className="App">
-     
+      <Container>
+        <Grid container spacing={3}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,13 @@
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+
+
+const TourCard =()=>{
+    return (
+     <Grid item xs={3}>
+     <Paper elevation ="3">Hello</Paper>
+    </Grid>   
+)
+    
+}
+export default TourCard
