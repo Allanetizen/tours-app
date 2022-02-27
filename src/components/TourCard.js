a21d2ce285007400dedfd2926903075343122643
Allan <allankosgei747@gmail.com>
2022-02-27 15:19:33 +0300

    image

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -5,7 +5,9 @@ import Grid from '@mui/material/Grid'
 const TourCard =()=>{
     return (
      <Grid item xs={3}>
-     <Paper elevation ="3">Hello</Paper>
+     <Paper elevation ="3">
+         <img src="https://cdn.pixabay.com/photo/2021/08/04/14/16/tower-6521842_1280.jpg" alt="" className="img" />
+     </Paper>
     </Grid>   
 )
     
