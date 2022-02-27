0d178be2a578002a24a999c67c5183c4573c5c28
Allan <allankosgei747@gmail.com>
2022-02-27 17:18:56 +0300

    added icons, rating

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -2,6 +2,8 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import {AccessTime} from "@mui/icons-material/"
+import Rating from "@mui/material/Rating";
 
 const TourCard = () => {
   return (
@@ -21,9 +23,36 @@ const TourCard = () => {
               display: "flex",
               alignItems: "center",
             }}
-          >
-            <Typography variant="body2" component="paragraph">
+          ><AccessTime sx={{
+              width:12.5
+          }}/>
+            <Typography variant="body2" component="paragraph" marginLeft={0.5}>
               5 hours
+            </Typography>
+          </Box>
+          <Box sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={3}
+            >
+            <Rating name="read-only" value={4.5} precision ="0.5" readOnly size="small"/>
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+             4.5
+                
+            </Typography> 
+            
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+             (456 reviews )
+                
+            </Typography>
+          
+            
+          </Box>
+          <Box>
+          <Typography variant="h6" component="h3" marginTop={0}>
+             From US $100
+                
             </Typography>
           </Box>
         </Box>
