e70f863905c31bb6faf1e919235265e0d474e22c
Allan <allankosgei747@gmail.com>
2022-02-28 16:46:02 +0300

    destructured the app.js code to => home.js page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import "./App.css";
-import TourCard from "./components/TourCard";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import SearchAppBar from './components/AppBar';
-import countries from "./data.json"
-import { Typography } from "@mui/material";
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      <SearchAppBar/>
-      <Container  sx={{
-        marginY:5
-      }}>
-        {countries.map((country)=>(
-          <>
-<Typography 
-  variant ="h4"
-  component="h2"
-  marginTop={5}
-  marginBottom={3}>
-
-  Top Tour Destinations in {country.name}
-
-</Typography>
-<Grid container spacing={3}>
-  {country.tours.map((tour, index)=>(
-     <TourCard tour={tour} key={index} />
-  ))}
-         
-          
-        </Grid>
-
-</>
-
-        ))}
-        
-      </Container>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import TourCard from "../components/TourCard";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import SearchAppBar from "../components/AppBar";
+import countries from "../data.json";
+import { Typography } from "@mui/material";
+
+const Home = () => (
+  <div className="App">
+    <SearchAppBar />
+    <Container
+      sx={{
+        marginY: 5,
+      }}
+    >
+      {countries.map((country) => (
+        <>
+          <Typography
+            variant="h4"
+            component="h2"
+            marginTop={5}
+            marginBottom={3}
+          >
+            Top Tour Destinations in {country.name}
+          </Typography>
+          <Grid container spacing={3}>
+            {country.tours.map((tour, index) => (
+              <TourCard tour={tour} key={index} />
+            ))}
+          </Grid>
+        </>
+      ))}
+    </Container>
+  </div>
+);
+export default Home;
