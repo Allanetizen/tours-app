c3845e6cf3eb3db38556d860e969572363b22551
Allan <allankosgei747@gmail.com>
2022-02-28 22:19:16 +0300

    introduced a tour page, to display collage of destinations

---
+++ src/pages/Tour.js
@@ -0,0 +1,15 @@
+import Container from "@mui/material/Container"
+import Typography from '@mui/material/Typography'
+
+
+const Tour =()=> <Container sx={{
+ width:900   
+}}>
+    <Typography variant="h3" component="h1" marginTop ="3" >
+        Explore East Africa
+    </Typography>
+
+</Container>
+
+
+export default Tour
